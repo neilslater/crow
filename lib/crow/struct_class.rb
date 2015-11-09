5f0ff435e9565d7b37c48d0450b31f8661fd2eae
Neil Slater <slobo777@gmail.com>
2015-11-09 23:37:23 +0000

    More documentation

--- lib/crow/struct_class.rb
+++ lib/crow/struct_class.rb
@@ -122,12 +122,56 @@ module Crow
     end
   end
 
+  # This class models the description of dual Ruby class and C struct code.
+  #
+  # An object of the class describes a C struct, and its Ruby representation.
+  #
+  # NB There are limits to flexibility in the descriptions.
+  #
+  # @example Define a basic C struct with two attributes and write its files to a folder
+  #  structdef = Crow::StructClass.new( 'the_class',
+  #    :attributes => [{:name=>'number',:ctype=>:int}, {:name=>'values',:ctype=>:double,:pointer=>true}] } ] )
+  #  structdef.write( '/path/to/target_project/ext/the_module' )
+  #
   class StructClass
-    attr_accessor :short_name, :struct_name, :rb_class_name, :attributes, :parent_lib, :init_params
+    # The label used for file names and struct pointers relating to this struct
+    # @return [String]
+    attr_accessor :short_name
+
+    # The label used for struct type (by default derived from short_name)
+    # @return [String]
+    attr_accessor :struct_name
+
+    # The name used for Ruby Class wrapper for this type (by default derived from short_name)
+    # @return [String]
+    attr_accessor :rb_class_name
+
+    # List of attributes defined in the struct and class
+    # @return [Array<Crow::TypeMap>]
+    attr_accessor :attributes
+
+    # The container library for the struct. Although you can generate struct wrappers without this being set,
+    # some templated code needs to know the correct container.
+    # @return [Crow::LibDef]
+    attr_accessor :parent_lib
+
+    # List of params used to initialize a struct or class of this type
+    # @return [Array<Crow::TypeMap>]
+    attr_accessor :init_params
 
     TEMPLATE_DIR = File.realdirpath( File.join( __dir__, '../../lib/templates' ) )
     TEMPLATES = [ 'struct_dataset.h', 'struct_dataset.c', 'ruby_class_dataset.h', 'ruby_class_dataset.c' ]
 
+    # Creates a new struct description.
+    # @param [String] short_name identifying name for struct and class
+    # @param [Hash] opts
+    # @option opts [String] :struct_name if provided then over-rides name automatically derived from short_name
+    # @option opts [String] :rb_class_name if provided then over-rides name automatically derived from short_name
+    # @option opts [Array<Hash>] :attributes, if provided these are used to create new Crow::TypeMap objects that describe attributes
+    # @option opts [Array<Hash>] :init_params, if provided these are used to create new Crow::TypeMap objects that describe params to initialise of the new class
+    # @option opts [Crow::LibDef] :parent_lib, if provided then sets parent_lib
+    # @return [Crow::StructClass]
+    #
     def initialize( short_name, opts = {} )
       raise "Short name '#{short_name}' cannot be used" if short_name !~ /\A[a-zA-Z0-9_]+\z/
       @short_name = short_name
@@ -155,22 +199,35 @@ module Crow
       @parent_lib = opts[:parent_lib] || LibDef.new( 'module' )
     end
 
+    # Writes four C source files that implement a basic Ruby native extension for the class. The files
+    # are split into a Ruby class binding and a struct definition, each of which has a .c and .h file.
+    # @param [String] path directory to write files to.
+    # @return [true]
     def write path
       TEMPLATES.each do |template|
         File.open( File.join( path, template.sub( /dataset/, short_name ) ), 'w' ) do |file|
           file.puts render( File.join( TEMPLATE_DIR, template ) )
         end
       end
+      true
     end
 
+    # Adds an attribute definition to the struct/class description.
+    # @param [String] name identifier used for the attribute in C and Ruby references
+    # @param [Hash] opts passed to Crow::TypeMap constructor
+    # @return [Crow::TypeMap] the new attribute definition
     def add_attribute name, opts = {}
       @attributes << TypeMap.create( name, opts )
     end
 
+    # Whether any of the attributes are NArray objects.
+    # @return [Boolean]
     def any_narray?
       @attributes.any? { |a| a.is_narray? }
     end
 
+    # List of attributes which contain NArray objects.
+    # @return [Array<Crow::TypeMap>]
     def narray_attributes
       @attributes.select { |a| a.is_narray? }
     end
