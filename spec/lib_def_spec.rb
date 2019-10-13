47e886166e774b9faca9559bdc19f4dbcb14ed86
Neil Slater <slobo777@gmail.com>
2019-10-13 21:18:31 +0100

    Add fourth type

--- spec/lib_def_spec.rb
+++ spec/lib_def_spec.rb
@@ -41,6 +41,7 @@ describe Crow::LibDef do
             { name: 'narr_data', ruby_name: 'data', ctype: :NARRAY_DOUBLE, rank_expr: '2', shape_exprs: [ '$width', '$height' ] },
             { name: 'narr_summary', ruby_name: 'summary', ctype: :NARRAY_DOUBLE, rank_expr: '1', shape_exprs: [ '$width' ], shape_var: 'summary_shape' },
             { name: 'narr_counts', ruby_name: 'counts', ctype: :NARRAY_INT_32, rank_expr: '1', shape_exprs: [ '$height' ], ptr_cache: 'counts' },
+            { name: 'narr_inverse', ruby_name: 'inverse', ctype: :NARRAY_FLOAT, rank_expr: '2', shape_exprs: [ '$height', '$width' ], ptr_cache: 'inverse', shape_var: 'inverse_shape' },
           ],
           init_params: [{name: 'width', ctype: :int}, {name: 'height', ctype: :int}]
         }
