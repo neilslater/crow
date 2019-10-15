e0129c4fc2faca5b78d370113cd1fa2b7436bf88
Neil Slater <slobo777@gmail.com>
2019-10-15 21:53:41 +0100

    Extend templated spec a little

--- lib/templates/spec/dataset_spec.rb
+++ lib/templates/spec/dataset_spec.rb
@@ -4,4 +4,10 @@ describe <%= full_class_name_ruby %> do
   it "is a valid Class" do
     expect(<%= full_class_name_ruby %>).to be_a Class
   end
+
+  describe "#new" do
+    it "instantiates a <%= full_class_name_ruby %>" do
+      expect( <%= full_class_name_ruby %>.new( <%= init_params.map{ '1' }.join(', ') %> ) ).to be_a <%= full_class_name_ruby %>
+    end
+  end
 end
