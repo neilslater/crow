40361c9ce1d86bcabcdc5c8fc9c12e714ed500ef
Neil Slater <slobo777@gmail.com>
2020-07-23 13:16:46 +0100

    More Rubocop fixes

--- Rakefile
+++ Rakefile
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'bundler/gem_tasks'
 require 'rspec/core/rake_task'
 require 'fileutils'
@@ -20,6 +22,60 @@ RSpec::Core::RakeTask.new(:test) do |t|
   t.verbose = false
 end
 
+def demo_structs
+  [
+    demo_bar,
+    demo_baz,
+    demo_table
+  ]
+end
+
+def demo_bar
+  {
+    name: 'bar',
+    attributes: [
+      { name: 'hi', ctype: :int, ruby_write: true, init: { expr: '.' } }
+    ],
+    init_params: [{ name: 'hi', ctype: :int }]
+  }
+end
+
+def demo_baz
+  {
+    name: 'baz',
+    attributes: [
+      { name: 'num_things', ctype: :int, init: { expr: '.' } },
+      { name: 'things', ctype: :int, pointer: true, ruby_read: false,
+        init: { size_expr: '.num_things', expr: '0' } }
+    ],
+    init_params: [{ name: 'num_things', ctype: :int }]
+  }
+end
+
+def demo_table
+  {
+    name: 'table',
+    attributes: demo_table_attributes,
+    init_params: [
+      { name: 'width', ctype: :int, init: { validate_min: 1, validate_max: 10 } },
+      { name: 'height', ctype: :int, init: { validate_min: 1, validate_max: 20 } }
+    ]
+  }
+end
+
+def demo_table_attributes
+  [
+    { name: 'narr_data', ruby_name: 'data', ctype: :NARRAY_DOUBLE,
+      init: { rank_expr: '2', shape_exprs: ['$width', '$height'] } },
+    { name: 'narr_summary', ruby_name: 'summary', ctype: :NARRAY_DOUBLE,
+      init: { rank_expr: '1', shape_exprs: ['$width'] } },
+    { name: 'narr_counts', ruby_name: 'counts', ctype: :NARRAY_INT_32,
+      init: { rank_expr: '1', shape_exprs: ['$height'] } },
+    { name: 'narr_inverse', ruby_name: 'inverse', ctype: :NARRAY_FLOAT,
+      init: { rank_expr: '2', shape_exprs: ['$height', '$width'] } }
+  ]
+end
+
 desc 'Create a test project'
 task :demo, :out_path do |_t, args|
   out_path = args[:out_path]
@@ -35,36 +91,7 @@ task :demo, :out_path do |_t, args|
 
   l = Crow::LibDef.new(
     'foo',
-    structs: [
-      {
-        name: 'bar',
-        attributes: [
-          { name: 'hi', ctype: :int, ruby_write: true, init: { expr: '.' } }
-        ],
-        init_params: [{ name: 'hi', ctype: :int }]
-      },
-      {
-        name: 'baz',
-        attributes: [
-          { name: 'num_things', ctype: :int, init: { expr: '.' } },
-          { name: 'things', ctype: :int, pointer: true, ruby_read: false, init: { size_expr: '.num_things', expr: '0' } }
-        ],
-        init_params: [{ name: 'num_things', ctype: :int }]
-      },
-      {
-        name: 'table',
-        attributes: [
-          { name: 'narr_data', ruby_name: 'data', ctype: :NARRAY_DOUBLE, init: { rank_expr: '2', shape_exprs: ['$width', '$height'] } },
-          { name: 'narr_summary', ruby_name: 'summary', ctype: :NARRAY_DOUBLE, init: { rank_expr: '1', shape_exprs: ['$width'] } },
-          { name: 'narr_counts', ruby_name: 'counts', ctype: :NARRAY_INT_32, init: { rank_expr: '1', shape_exprs: ['$height'] } },
-          { name: 'narr_inverse', ruby_name: 'inverse', ctype: :NARRAY_FLOAT, init: { rank_expr: '2', shape_exprs: ['$height', '$width'] } }
-        ],
-        init_params: [
-          { name: 'width', ctype: :int, init: { validate_min: 1, validate_max: 10 } },
-          { name: 'height', ctype: :int, init: { validate_min: 1, validate_max: 20 } }
-        ]
-      }
-    ]
+    structs: demo_structs
   )
 
   l.create_project(out_path)
--- lib/crow/typemap_basic_types.rb
+++ lib/crow/typemap_basic_types.rb
@@ -1,181 +1,191 @@
 # frozen_string_literal: true
 
 module Crow
-  class TypeMap::Int < TypeMap
-    include NotACPointer
-    self.default = '0'
+  class TypeMap
+    # Describes a C `int` data type.
+    class Int < TypeMap
+      include NotACPointer
+      self.default = '0'
 
-    def cbase
-      'int'
+      def cbase
+        'int'
+      end
+
+      def rdoc_type
+        'Integer'
+      end
+
+      def self.ruby_to_c(ruby_name)
+        "NUM2INT( #{ruby_name} )"
+      end
+
+      def self.c_to_ruby(c_name)
+        "INT2NUM( #{c_name} )"
+      end
     end
 
-    def rdoc_type
-      'Integer'
+    # Describes a C `unsigned int` data type.
+    class UInt < TypeMap
+      include NotACPointer
+
+      self.default = '0'
+
+      def cbase
+        'unsigned int'
+      end
+
+      def rdoc_type
+        'Integer'
+      end
+
+      def self.ruby_to_c(ruby_name)
+        "NUM2UINT( #{ruby_name} )"
+      end
+
+      def self.c_to_ruby(c_name)
+        "UINT2NUM( #{c_name} )"
+      end
     end
 
-    def self.ruby_to_c(ruby_name)
-      "NUM2INT( #{ruby_name} )"
+    # Describes a C `long` data type.
+    class Long < TypeMap
+      include NotACPointer
+
+      self.default = '0L'
+
+      def cbase
+        'long'
+      end
+
+      def rdoc_type
+        'Integer'
+      end
+
+      def self.ruby_to_c(ruby_name)
+        "NUM2LONG( #{ruby_name} )"
+      end
+
+      def self.c_to_ruby(c_name)
+        "LONG2NUM( #{c_name} )"
+      end
     end
 
-    def self.c_to_ruby(c_name)
-      "INT2NUM( #{c_name} )"
-    end
-  end
+    # Describes a C `unsigned long` data type.
+    class ULong < TypeMap::Long
+      include NotACPointer
+      self.default = '0L'
 
-  class TypeMap::UInt < TypeMap::Int
-    include NotACPointer
+      def cbase
+        'unsigned long'
+      end
 
-    self.default = '0'
+      def rdoc_type
+        'Integer'
+      end
 
-    def cbase
-      'unsigned int'
-    end
+      def self.ruby_to_c(ruby_name)
+        "NUM2ULONG( #{ruby_name} )"
+      end
 
-    def rdoc_type
-      'Integer'
-    end
-
-    def self.ruby_to_c(ruby_name)
-      "NUM2UINT( #{ruby_name} )"
-    end
-
-    def self.c_to_ruby(c_name)
-      "UINT2NUM( #{c_name} )"
-    end
-  end
-
-  class TypeMap::Long < TypeMap
-    include NotACPointer
-
-    self.default = '0L'
-
-    def cbase
-      'long'
+      def self.c_to_ruby(c_name)
+        "ULONG2NUM( #{c_name} )"
+      end
     end
 
-    def rdoc_type
-      'Integer'
+    # Describes a C `float` data type.
+    class Float < TypeMap
+      include NotACPointer
+      self.default = '0.0'
+
+      def cbase
+        'float'
+      end
+
+      def rdoc_type
+        'Float'
+      end
+
+      def self.ruby_to_c(ruby_name)
+        "NUM2FLT( #{ruby_name} )"
+      end
+
+      def self.c_to_ruby(c_name)
+        "FLT2NUM( #{c_name} )"
+      end
     end
 
-    def self.ruby_to_c(ruby_name)
-      "NUM2LONG( #{ruby_name} )"
+    # Describes a C `double` data type.
+    class Double < TypeMap
+      include NotACPointer
+      self.default = '0.0'
+
+      def cbase
+        'double'
+      end
+
+      def rdoc_type
+        'Float'
+      end
+
+      def self.ruby_to_c(ruby_name)
+        "NUM2DBL( #{ruby_name} )"
+      end
+
+      def self.c_to_ruby(c_name)
+        "DBL2NUM( #{c_name} )"
+      end
     end
 
-    def self.c_to_ruby(c_name)
-      "LONG2NUM( #{c_name} )"
-    end
-  end
+    # Describes a C `char` data type.
+    class Char < TypeMap
+      include NotACPointer
+      self.default = '0'
 
-  class TypeMap::ULong < TypeMap::Long
-    include NotACPointer
-    self.default = '0L'
+      def cbase
+        'char'
+      end
 
-    def cbase
-      'unsigned long'
-    end
+      def rdoc_type
+        'Byte'
+      end
 
-    def rdoc_type
-      'Integer'
-    end
+      def self.ruby_to_c(ruby_name)
+        "NUM2CHR( #{ruby_name} )"
+      end
 
-    def self.ruby_to_c(ruby_name)
-      "NUM2ULONG( #{ruby_name} )"
-    end
-
-    def self.c_to_ruby(c_name)
-      "ULONG2NUM( #{c_name} )"
-    end
-  end
-
-  class TypeMap::Float < TypeMap
-    include NotACPointer
-    self.default = '0.0'
-
-    def cbase
-      'float'
+      def self.c_to_ruby(c_name)
+        "LONG2FIX( #{c_name} )"
+      end
     end
 
-    def rdoc_type
-      'Float'
-    end
+    # Describes a C `VALUE` data type from `ruby.h`, i.e. a Ruby Object pointer.
+    class Value < TypeMap
+      include NotACPointer
+      self.default = 'Qnil'
 
-    def self.ruby_to_c(ruby_name)
-      "NUM2FLT( #{ruby_name} )"
-    end
+      def cbase
+        'volatile VALUE'
+      end
 
-    def self.c_to_ruby(c_name)
-      "FLT2NUM( #{c_name} )"
-    end
-  end
+      def needs_gc_mark?
+        true
+      end
 
-  class TypeMap::Double < TypeMap
-    include NotACPointer
-    self.default = '0.0'
+      def cast
+        'ERROR'
+      end
 
-    def cbase
-      'double'
-    end
+      def rdoc_type
+        'Object'
+      end
 
-    def rdoc_type
-      'Float'
-    end
+      def self.ruby_to_c(ruby_name)
+        ruby_name
+      end
 
-    def self.ruby_to_c(ruby_name)
-      "NUM2DBL( #{ruby_name} )"
-    end
-
-    def self.c_to_ruby(c_name)
-      "DBL2NUM( #{c_name} )"
-    end
-  end
-
-  class TypeMap::Char < TypeMap
-    include NotACPointer
-    self.default = '0'
-
-    def cbase
-      'char'
-    end
-
-    def rdoc_type
-      'Byte'
-    end
-
-    def self.ruby_to_c(ruby_name)
-      "NUM2CHR( #{ruby_name} )"
-    end
-
-    def self.c_to_ruby(c_name)
-      "LONG2FIX( #{c_name} )"
-    end
-  end
-
-  class TypeMap::Value < TypeMap
-    include NotACPointer
-    self.default = 'Qnil'
-
-    def cbase
-      'volatile VALUE'
-    end
-
-    def needs_gc_mark?
-      true
-    end
-
-    def cast
-      'ERROR'
-    end
-
-    def rdoc_type
-      'Object'
-    end
-
-    def self.ruby_to_c(ruby_name)
-      ruby_name
-    end
-
-    def self.c_to_ruby(c_name)
-      c_name
+      def self.c_to_ruby(c_name)
+        c_name
+      end
     end
   end
 end
