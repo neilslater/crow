ea78f358ccadd571eb2cb227dd6d15ae122cf925
Neil Slater <slobo777@gmail.com>
2020-07-23 13:24:54 +0100

    Rubocop for TypeMap subclasses

--- lib/crow/typemap_narray.rb
+++ lib/crow/typemap_narray.rb
@@ -1,136 +1,138 @@
 # frozen_string_literal: true
 
 module Crow
-  class TypeMap::NArray < TypeMap::Value
-    include NotACPointer
-    self.default = 'Qnil'
+  class TypeMap
+    class NArray < TypeMap::Value
+      include NotACPointer
+      self.default = 'Qnil'
 
-    def shape_tmp_var
-      @shape_tmp_var ||= @parent_struct.short_name + '_' + @name + '_shape'
+      def shape_tmp_var
+        @shape_tmp_var ||= @parent_struct.short_name + '_' + @name + '_shape'
+      end
+
+      def ptr_tmp_var
+        @ptr_tmp_var ||= @parent_struct.short_name + '_' + @name + '_ptr'
+      end
+
+      def init_class
+        Crow::TypeInit::NArray
+      end
+
+      def narray?
+        true
+      end
+
+      def rdoc_type
+        'NArray'
+      end
+
+      def declare_ptr_cache
+        "#{item_ctype} *#{ptr_tmp_var};"
+      end
+
+      def declare_shape_var
+        "int *#{shape_tmp_var};"
+      end
+
+      def set_ptr_cache
+        "#{ptr_tmp_var} = #{ptr_fn_name}( #{@parent_struct.short_name} );"
+      end
+
+      def set_shape_var
+        "#{shape_tmp_var} = #{shape_fn_name}( #{@parent_struct.short_name} );"
+      end
+
+      def narray_fn_name
+        "#{@parent_struct.short_name}__get_#{name}_NARRAY"
+      end
+
+      def size_fn_name
+        "#{@parent_struct.short_name}__get_#{name}_size"
+      end
+
+      def rank_fn_name
+        "#{@parent_struct.short_name}__get_#{name}_rank"
+      end
+
+      def ptr_fn_name
+        "#{@parent_struct.short_name}__get_#{name}_ptr"
+      end
+
+      def shape_fn_name
+        "#{@parent_struct.short_name}__get_#{name}_shape"
+      end
     end
 
-    def ptr_tmp_var
-      @ptr_tmp_var ||= @parent_struct.short_name + '_' + @name + '_ptr'
+    class NArrayFloat < TypeMap::NArray
+      include NotACPointer
+      self.default = 'Qnil'
+      self.item_default = '0.0'
+
+      def item_ctype
+        'float'
+      end
+
+      def narray_enum_type
+        'NA_SFLOAT'
+      end
+
+      def rdoc_type
+        'NArray<sfloat>'
+      end
     end
 
-    def init_class
-      Crow::TypeInit::NArray
+    class NArrayDouble < TypeMap::NArray
+      include NotACPointer
+      self.default = 'Qnil'
+      self.item_default = '0.0'
+
+      def item_ctype
+        'double'
+      end
+
+      def narray_enum_type
+        'NA_DFLOAT'
+      end
+
+      def rdoc_type
+        'NArray<float>'
+      end
     end
 
-    def narray?
-      true
+    class NArraySInt < TypeMap::NArray
+      include NotACPointer
+      self.default = 'Qnil'
+      self.item_default = '0'
+
+      def item_ctype
+        'int16_t'
+      end
+
+      def narray_enum_type
+        'NA_SINT'
+      end
+
+      def rdoc_type
+        'NArray<sint>'
+      end
     end
 
-    def rdoc_type
-      'NArray'
-    end
+    class NArrayLInt < TypeMap::NArray
+      include NotACPointer
+      self.default = 'Qnil'
+      self.item_default = '0'
 
-    def declare_ptr_cache
-      "#{item_ctype} *#{ptr_tmp_var};"
-    end
+      def item_ctype
+        'int32_t'
+      end
 
-    def declare_shape_var
-      "int *#{shape_tmp_var};"
-    end
+      def narray_enum_type
+        'NA_LINT'
+      end
 
-    def set_ptr_cache
-      "#{ptr_tmp_var} = #{ptr_fn_name}( #{@parent_struct.short_name} );"
-    end
-
-    def set_shape_var
-      "#{shape_tmp_var} = #{shape_fn_name}( #{@parent_struct.short_name} );"
-    end
-
-    def narray_fn_name
-      "#{@parent_struct.short_name}__get_#{name}_NARRAY"
-    end
-
-    def size_fn_name
-      "#{@parent_struct.short_name}__get_#{name}_size"
-    end
-
-    def rank_fn_name
-      "#{@parent_struct.short_name}__get_#{name}_rank"
-    end
-
-    def ptr_fn_name
-      "#{@parent_struct.short_name}__get_#{name}_ptr"
-    end
-
-    def shape_fn_name
-      "#{@parent_struct.short_name}__get_#{name}_shape"
-    end
-  end
-
-  class TypeMap::NArrayFloat < TypeMap::NArray
-    include NotACPointer
-    self.default = 'Qnil'
-    self.item_default = '0.0'
-
-    def item_ctype
-      'float'
-    end
-
-    def narray_enum_type
-      'NA_SFLOAT'
-    end
-
-    def rdoc_type
-      'NArray<sfloat>'
-    end
-  end
-
-  class TypeMap::NArrayDouble < TypeMap::NArray
-    include NotACPointer
-    self.default = 'Qnil'
-    self.item_default = '0.0'
-
-    def item_ctype
-      'double'
-    end
-
-    def narray_enum_type
-      'NA_DFLOAT'
-    end
-
-    def rdoc_type
-      'NArray<float>'
-    end
-  end
-
-  class TypeMap::NArraySInt < TypeMap::NArray
-    include NotACPointer
-    self.default = 'Qnil'
-    self.item_default = '0'
-
-    def item_ctype
-      'int16_t'
-    end
-
-    def narray_enum_type
-      'NA_SINT'
-    end
-
-    def rdoc_type
-      'NArray<sint>'
-    end
-  end
-
-  class TypeMap::NArrayLInt < TypeMap::NArray
-    include NotACPointer
-    self.default = 'Qnil'
-    self.item_default = '0'
-
-    def item_ctype
-      'int32_t'
-    end
-
-    def narray_enum_type
-      'NA_LINT'
-    end
-
-    def rdoc_type
-      'NArray<int>'
+      def rdoc_type
+        'NArray<int>'
+      end
     end
   end
 end
--- lib/crow/typemap_pointers.rb
+++ lib/crow/typemap_pointers.rb
@@ -1,153 +1,155 @@
 # frozen_string_literal: true
 
 module Crow
-  class TypeMap::P_Int < TypeMap
-    include IsACPointer
-    self.default = 'NULL'
-    self.item_default = '0'
+  class TypeMap
+    class P_Int < TypeMap
+      include IsACPointer
+      self.default = 'NULL'
+      self.item_default = '0'
 
-    def cbase
-      'int'
+      def cbase
+        'int'
+      end
+
+      def rdoc_type
+        'Array<Integer>'
+      end
+
+      def array_item_to_ruby_converter
+        'INT2NUM'
+      end
+
+      def self.store_default
+        false
+      end
     end
 
-    def rdoc_type
-      'Array<Integer>'
+    class P_UInt < TypeMap
+      include IsACPointer
+      self.default = 'NULL'
+      self.item_default = '0'
+
+      def cbase
+        'unsigned int'
+      end
+
+      def rdoc_type
+        'Array<Integer>'
+      end
+
+      def array_item_to_ruby_converter
+        'UINT2NUM'
+      end
+
+      def self.store_default
+        false
+      end
     end
 
-    def array_item_to_ruby_converter
-      'INT2NUM'
+    class P_Long < TypeMap
+      include IsACPointer
+      self.default = 'NULL'
+      self.item_default = '0L'
+
+      def cbase
+        'long'
+      end
+
+      def rdoc_type
+        'Integer'
+      end
+
+      def array_item_to_ruby_converter
+        'LONG2NUM'
+      end
+
+      def self.store_default
+        false
+      end
     end
 
-    def self.store_default
-      false
-    end
-  end
+    class P_ULong < TypeMap
+      include IsACPointer
+      self.default = 'NULL'
+      self.item_default = '0L'
 
-  class TypeMap::P_UInt < TypeMap
-    include IsACPointer
-    self.default = 'NULL'
-    self.item_default = '0'
+      def cbase
+        'unsigned long'
+      end
 
-    def cbase
-      'unsigned int'
-    end
+      def rdoc_type
+        'Array<Integer>'
+      end
 
-    def rdoc_type
-      'Array<Integer>'
-    end
+      def array_item_to_ruby_converter
+        'ULONG2NUM'
+      end
 
-    def array_item_to_ruby_converter
-      'UINT2NUM'
-    end
-
-    def self.store_default
-      false
-    end
-  end
-
-  class TypeMap::P_Long < TypeMap
-    include IsACPointer
-    self.default = 'NULL'
-    self.item_default = '0L'
-
-    def cbase
-      'long'
+      def self.store_default
+        false
+      end
     end
 
-    def rdoc_type
-      'Integer'
+    class P_Float < TypeMap
+      include IsACPointer
+      self.default = 'NULL'
+      self.item_default = '0.0'
+
+      def cbase
+        'float'
+      end
+
+      def rdoc_type
+        'Array<Float>'
+      end
+
+      def array_item_to_ruby_converter
+        'FLT2NUM'
+      end
+
+      def self.store_default
+        false
+      end
     end
 
-    def array_item_to_ruby_converter
-      'LONG2NUM'
+    class P_Double < TypeMap
+      include IsACPointer
+      self.default = 'NULL'
+      self.item_default = '0.0'
+
+      def cbase
+        'double'
+      end
+
+      def rdoc_type
+        'Array<Float>'
+      end
+
+      def array_item_to_ruby_converter
+        'DBL2NUM'
+      end
+
+      def self.store_default
+        false
+      end
     end
 
-    def self.store_default
-      false
-    end
-  end
+    class P_Char < TypeMap
+      include IsACPointer
+      self.default = 'NULL'
+      self.item_default = '0'
 
-  class TypeMap::P_ULong < TypeMap
-    include IsACPointer
-    self.default = 'NULL'
-    self.item_default = '0L'
+      def cbase
+        'char'
+      end
 
-    def cbase
-      'unsigned long'
-    end
+      def rdoc_type
+        'String'
+      end
 
-    def rdoc_type
-      'Array<Integer>'
-    end
-
-    def array_item_to_ruby_converter
-      'ULONG2NUM'
-    end
-
-    def self.store_default
-      false
-    end
-  end
-
-  class TypeMap::P_Float < TypeMap
-    include IsACPointer
-    self.default = 'NULL'
-    self.item_default = '0.0'
-
-    def cbase
-      'float'
-    end
-
-    def rdoc_type
-      'Array<Float>'
-    end
-
-    def array_item_to_ruby_converter
-      'FLT2NUM'
-    end
-
-    def self.store_default
-      false
-    end
-  end
-
-  class TypeMap::P_Double < TypeMap
-    include IsACPointer
-    self.default = 'NULL'
-    self.item_default = '0.0'
-
-    def cbase
-      'double'
-    end
-
-    def rdoc_type
-      'Array<Float>'
-    end
-
-    def array_item_to_ruby_converter
-      'DBL2NUM'
-    end
-
-    def self.store_default
-      false
-    end
-  end
-
-  class TypeMap::P_Char < TypeMap
-    include IsACPointer
-    self.default = 'NULL'
-    self.item_default = '0'
-
-    def cbase
-      'char'
-    end
-
-    def rdoc_type
-      'String'
-    end
-
-    def self.store_default
-      false
+      def self.store_default
+        false
+      end
     end
   end
 end
